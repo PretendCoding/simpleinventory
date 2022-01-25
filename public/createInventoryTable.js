89e2eb73f51bf36475832c45f829468a39874baf
Aaron <awinebrenner@gmail.com>
2022-01-25 17:56:30 -0600

    Updating styles

--- public/createInventoryTable.js
+++ public/createInventoryTable.js
@@ -3,22 +3,33 @@ import { showUpdateModal } from "./updateItem.js";
 // This function should take in a JSON object which contains the list of inventory objects. This function should only be concerned with creating the HTML table
 
 export function createTable(data) {
+    
+    // let thead = document.querySelector('thead');
 
-    const oldTable = document.querySelector('table');
+    // if (thead === null) {
+    //     let theadTable = document.body.appendChild(document.createElement('table'));
+    //     theadTable.classList.add('inventory-table');
+    //     thead = theadTable.appendChild(document.createElement('thead'));
+    //     const trHead = thead.appendChild(document.createElement('tr'));
+    //     trHead.innerHTML = '<th>Item ID</th><th>Item Name</th><th>Unit</th><th>Quantity</th>';
+    //     console.log();
+    // }
+
+    const oldTable = document.getElementById('mainInventoryTable');
+
+    let table;
 
     if (oldTable !== null) {
         oldTable.innerHTML = '';
+        table = oldTable;
+    } else {        
+        let myDiv = document.getElementsByClassName('tbl-content')[0];
+        table = myDiv.appendChild(document.createElement('table'));
+        table.id = "mainInventoryTable";
+        table.classList.add('inventory-table');
     }
-
-    const table = document.body.appendChild(document.createElement('table'));
-    table.classList.add('inventory-table');
-
-    const thead = table.appendChild(document.createElement('thead'));
+    
     const tbody = table.appendChild(document.createElement('tbody'));
-
-    const trHead = thead.appendChild(document.createElement('tr'));
-
-    thead.innerHTML = '<th>Item ID</th><th>Item Name</th><th>Unit</th><th>Quantity</th>';
 
     data.forEach(element => { addItemToTable(element, tbody); });
 }
