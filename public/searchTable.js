d2abc7ad08a87e1358ffffeb7e01b2569d5301d7
Aaron <awinebrenner@gmail.com>
2022-02-05 18:00:51 -0600

    Oops... forgot to add the js file for the search

---
+++ public/searchTable.js
@@ -0,0 +1,31 @@
+// window.sessionStorage.tableData
+import { $ } from "./utilities.js";
+import { addItemToTable } from "./createInventoryTable.js";
+
+const stateChange = setInterval(() => {
+    if (document.readyState == 'complete'){
+        clearInterval(stateChange);
+        main();
+    }
+}, 100);
+
+function main() {
+    ////// Click Events
+
+    $('tableSearch').addEventListener('input', () => {
+        const table = JSON.parse(window.sessionStorage.tableData);
+        let data;
+
+        document.querySelector('tbody').innerHTML = '';
+
+        table.forEach(item => {
+
+            let value = $('tableSearch').value;
+
+            if (item.name.toLowerCase().includes(value.toLowerCase())) {
+                addItemToTable(item);
+            }
+        })
+
+    })
+}
